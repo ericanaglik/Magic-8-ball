68975e8640aea88b979e4f35ddb5f3c2594fb6b2
Erica Naglik <40673456+ericanaglik@users.noreply.github.com>
2018-12-20 21:08:12 -0500

    Added answers and shake button functionality

--- MagicEightBall/ViewController.swift
+++ MagicEightBall/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    let answers = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch","Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"]
+    
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var shakeButton: UIButton!
 
@@ -22,10 +26,17 @@ class ViewController: UIViewController {
         //Dispose of any resources that can be recreated.
     }
     
-    @IBAction func shakeButtonTapped(_ sender: Any){
-        print("Shake it like a polaroid picture!")
+    @IBAction func shakeButtonTapped(_ sender: UIButton){
+        //1
+        let maxIndex = UInt32(answers.count)
+        //2
+        let randomIndex  = Int(arc4random_uniform(maxIndex))
+        //3
+        answerLabel.text = answers[randomIndex]
         
-        answerLabel.text = "button was tapped"
+//        print("Shake it like a polaroid picture!")
+//
+//        answerLabel.text = "button was tapped"
     }
 
 
