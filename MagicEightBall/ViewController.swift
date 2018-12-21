1214ddfd882c0483e5c60d6f215a39a43b1087c8
Erica Naglik <40673456+ericanaglik@users.noreply.github.com>
2018-12-20 20:38:09 -0500

    Started on UI

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Erica Naglik on 12/20/18.
+//  Copyright © 2018 Erica Naglik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
