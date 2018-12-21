3b3a71bd9f9c003ff5fc193edc9fae56ec31f514
Erica Naglik <40673456+ericanaglik@users.noreply.github.com>
2018-12-20 20:56:11 -0500

    Add files via upload

--- MagicEightBall/ViewController.swift
+++ MagicEightBall/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var shakeButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        //Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func shakeButtonTapped(_ sender: Any){
+        print("Shake it like a polaroid picture!")
+        
+        answerLabel.text = "button was tapped"
     }
 
 
